9f878a0b51dfd3e49bbc8a2193005cf5de703148
quartz010 <quartz010@outlook.com>
2023-12-21 18:14:56 +0800

    Optimize directory structure

--- mysql_db/create_configmap.sh
+++
@@ -1,3 +0,0 @@
-echo -n MYSQL_ROOT_PASSWORD > mysql-root-password.txt
-kubectl create -n database-common secret generic mysql-root-password --from-file=password=./mysql-root-password.txt
-kubectl create -n database-common configmap mysql-config --from-file=./mysql.cnf 
